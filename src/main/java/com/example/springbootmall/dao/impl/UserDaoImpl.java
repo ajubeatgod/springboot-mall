fc77e85813420f47bd5aaefc45ce15e13a249023
ajubeatgod <ajubeatgod@gmail.com>
2023-06-08 16:37:31 +0800

    user function - implement register (check duplicate email, hide password)

--- src/main/java/com/example/springbootmall/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/springbootmall/dao/impl/UserDaoImpl.java
@@ -41,6 +41,26 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
+    public User getUserByEmail(String email) {
+        String sql = "SELECT user_id, email, password, created_date, last_modified_date " +
+                "FROM user WHERE email = :email";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
+
+        List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
+
+        User user;
+        if (CollectionUtils.isEmpty(userList)) {
+            user = null;
+        } else {
+            user = userList.get(0);
+        }
+
+        return user;
+    }
+
+    @Override
     public Integer createUser(UserRegisterRequest userRegisterRequest) {
         String sql = "INSERT INTO user(email, password, created_date, last_modified_date) " +
                 "VALUES (:email, :password, :createdDate, :lastModifiedDate)";
--- src/main/java/com/example/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,19 @@ import com.example.springbootmall.dao.UserDao;
 import com.example.springbootmall.dto.UserRegisterRequest;
 import com.example.springbootmall.model.User;
 import com.example.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +28,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        String email = userRegisterRequest.getEmail();
+
+        // check duplicate email
+        User user = userDao.getUserByEmail(email);
+
+        if (Objects.nonNull(user)) {
+            log.warn("Duplicate email: {}", email);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // create user
         return userDao.createUser(userRegisterRequest);
     }
 }
